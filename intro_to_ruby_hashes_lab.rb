4d0b0eb8d81fcd56e3e4d3618bb29384bfb668ce
Kyle Talmage <ktalmage85@gmail.com>
2020-02-04 23:29:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,7 +47,7 @@ def monopoly_with_fourth_tier
                 :reading_railroad => {"mortgage_value" => "$" + 100.to_s},
                 :shortline_railroad => {"mortgage_value" =>  "$" + 200.to_s},
                 :pennsylvania_railroad  => {"mortgage_value" => "$" + 200.to_s},
-                :b_and_o_railroad => {"mortgage_value" => "$" + 800.to_s}
+                :b_and_o_railroad => {"mortgage_value" => "$" + 400.to_s}
      }
    }
 }
